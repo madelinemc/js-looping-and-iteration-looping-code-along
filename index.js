205bee00ae0af8c5a335534afb0aaabeee06eb66
madelinemc <madeline.coons@gmail.com>
2021-01-08 22:34:32 -0600

    commit tree 4139a1ee991e0fc1df5a336fe79821bf699796da

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thanks = []
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thanks;
+}
+
+function countDown(n) {
+    let i = 0;
+    while (n >= i) {
+        console.log(n - i); i++;
+    }
+
+}
